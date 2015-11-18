931535472417f911789bb68e7d5837486a8bab1b
Ali Elouafiq <ali@ali.ma>
2015-11-18 11:49:26 +0000

    tracking user events

--- server/logic.js
+++ server/logic.js
@@ -1,3 +1,8 @@
+client_identifiers = {
+    ip_address : '127.0.0.1',
+    http_headers : undefined
+};
+
 serviceDiscovery = {
     server: 'http://127.0.0.1:3001/meteor',
     origin: '127.0.0.1',
@@ -25,12 +30,26 @@ Meteor.methods({
         // Make sure the user is logged in before inserting a message
 
         console.log([text, userId, username]);
+
         Conversation.insert({
             text: text,
             createdAt: new Date(),
             owner: userId,
             username: username
         });
+
+        var check = {
+            user_ip : client_identifiers.ip_address,
+            user_agent : client_identifiers.http_headers['user-agent'],
+            referrer : '',
+            is_test: 1,
+            comment_type : 'message',
+            comment_author : username,
+            comment_content : text
+        };
+
+        var isSpam = Meteor.call('akismetCheckSpam', check);
+        console.log(isSpam);
     },
     setChecked: function (taskId, setChecked) {
         var task = Conversation.findOne(taskId);
@@ -42,3 +61,13 @@ Meteor.methods({
         Conversation.update(taskId, { $set: { checked: setChecked} });
     }
 });
+
+function vanguard(connection){
+    var identifiers= {
+        ip_address: connection.clientAddress,
+        http_headers: connection.httpHeaders
+    };
+    console.log(identifiers);
+    client_identifiers = identifiers;
+}
+Meteor.onConnection(vanguard);
